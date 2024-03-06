b125e8b8f0a61ca49e18edb3e5f991276cd1af14
hung <hungn12333@gmail.com>
2024-03-06 19:25:31 +0700

    s

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,18 +1,26 @@
 import React from "react";
 
  class MyComponent extends React.Component {
+
     state = {
         name:"hung",
         age:'18',
         address:'213123'
     }
-
+    changeName= ()=>{
+        console.log(123123);
+        this.setState({
+            name:"asdasdsa"
+        })
+    }
 
     render(){
         return (
             <div>
                 my first component asd
                 my name is {this.state.name}
+
+                <button onClick={this.changeName}>click</button>
             </div>
         );
     }
