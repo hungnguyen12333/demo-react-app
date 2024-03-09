7849272173c96adf812b21fe848054c996dc4fc5
nguyen phi hung <hungn12333@gmail.com>
2024-03-09 12:58:11 +0700

    hook

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,31 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-class AddUser extends React.Component{
+// class AddUser extends React.Component{
 
-    onSubmit(e){
+//     state = {
+//         id: Math.floor(Math.random()*1000), 
+//         name: "name",
+//         age:13,
+//     }
+
+
+
+//     onSubmit(e){
+//         e.preventDefault();
+
+
+//         this.props.addUser({
+//             id:this.state.id,
+//             name:this.state.name,
+//             age:this.state.age
+//         })
+//     }
+
+//     onChangeInput(e){
+//         this.setState({
+//             id: Math.floor(Math.random()*100), 
+//             name:e.target.value
+//         })
+//     }
+//     render(){
+//         const {addUser} = this.props;
+
+//         return (
+//             <form onSubmit={(e)=>this.onSubmit(e)}>
+//                 <input type="text"  onChange={ (e)=>{this.onChangeInput(e)}} />
+//                 <button type="submit">submit</button>
+//             </form>
+//         );
+
+//     }
+
+
+
+// }
+    const AddUser  = (props) =>{
+    
+
+    const [user , setUser] = useState({
+        id: Math.floor(Math.random()*1000), 
+        name: "name",
+        age:13,
+    })
+
+    const onSubmit = (e)=>{
         e.preventDefault();
-
-        console.log(123);
-        this.props.addUser({
-            id:4,
-            name:"ád0",
-            age:123
+        props.addUser({
+            id:user.id,
+            name:user.name,
+            age:user.age
         })
     }
 
-
-    render(){
-        const {addUser} = this.props;
+    const onChangeInput=(e) => {
+        setUser({
+            id: Math.floor(Math.random()*100), 
+            name:e.target.value
+        })
+    }
+        const {addUser} = props;
 
         return (
-            <form onSubmit={(e)=>this.onSubmit(e)}>
-                <input type="text"/>
+            <form onSubmit={(e)=>onSubmit(e)}>
+                <input type="text"  onChange={ (e)=>{onChangeInput(e)}} />
                 <button type="submit">submit</button>
             </form>
         );
-
-    }
-
 
 
 }
--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
-class ListUser extends React.Component {
-
-
-
+// class ListUser extends React.Component {
     
-    render() {
-        const { listUsers  } = this.props;
-        return (
-            <div>
-               {listUsers.map((item)=>{
-                    return (
-                        <div key={item.id}>
-                            {item.id} + {item.name}+ {item.age}
+    
+//     render() {
+//         const { listUsers  } = this.props;
+//         return (
+//             <>
+
+//                 {true && 
+//                     <div>
+//                     {listUsers.map((item)=>{
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.id} + {item.name}+ {item.age}
+//                                 </div>
+//                             );
+//                     })}
+//                     </div>
+//                 }
+                
+//             </>
+
+//         );
+//     }
+    
+// }
+
+    const ListUser = (props) =>{
+            const { listUsers  } = props;
+            const [isShowHideListUser,setShowHidListUser] = useState(true);
+
+
+            const handleShowHideListUser = () =>{
+                setShowHidListUser(!isShowHideListUser)
+            }
+
+            return (
+                <>
+                    <span
+                    onClick={()=>{handleShowHideListUser()}}
+                    >
+                        Show list user
+                        {isShowHideListUser!==true ? "show" :"hide"}
+                    </span>
+                    {isShowHideListUser && 
+                        <div>
+                        {listUsers.map((item)=>{
+                                return (
+                                    <div key={item.id}>
+                                        {item.id} + {item.name}+ {item.age}
+                                    </div>
+                                );
+                        })}
                         </div>
-                    );
-
-               })}
-            </div>
-        );
-    }
+                    }
+                    
+                </>
     
-}
+            );
+        }
+
+    
+    
+    
 export default ListUser;
